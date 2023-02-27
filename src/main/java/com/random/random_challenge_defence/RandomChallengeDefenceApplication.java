f2ae3c8fa277bc7a64871940944e19718062295c
DA-WIT <totw5701@naver.com>
2023-02-27 21:32:52 +0900

    feat. 프로젝트 시작

---
+++ src/main/java/com/random/random_challenge_defence/RandomChallengeDefenceApplication.java
@@ -0,0 +1,13 @@
+package com.random.random_challenge_defence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomChallengeDefenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomChallengeDefenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/random/random_challenge_defence/RandomChallengeDefenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.random.random_challenge_defence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomChallengeDefenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
