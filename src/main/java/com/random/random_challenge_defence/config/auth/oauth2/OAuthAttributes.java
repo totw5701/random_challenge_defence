a0005d8e83e5e8118fb10dde3d6371365b98d1e9
totw5701 <inw2129@gmail.com>
2023-06-08 17:18:02 +0900

    kakao, naver social login 구현

--- src/main/java/com/random/random_challenge_defence/config/auth/oauth2/OAuthAttributes.java
+++ src/main/java/com/random/random_challenge_defence/config/auth/oauth2/OAuthAttributes.java
@@ -15,15 +15,15 @@ public class OAuthAttributes {
 
     private Map<String, Object> attributes;
     private String nameAttributeKey;
-    private String name;
+    private String nickname;
     private String email;
     private String picture;
 
     @Builder
-    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String nickname, String email, String picture) {
         this.attributes = attributes;
         this.nameAttributeKey = nameAttributeKey;
-        this.name = name;
+        this.nickname = nickname;
         this.email = email;
         this.picture = picture;
     }
@@ -31,14 +31,27 @@ public class OAuthAttributes {
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
         if("naver".equals(registrationId)) {
             return ofNaver("id", attributes);
+        } else if ("kakao".equals(registrationId)){
+            return ofKakao("id", attributes);
         }
-
         return ofGoogle(userNameAttributeName, attributes);
     }
 
+    private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+
+        return OAuthAttributes.builder()
+                .nickname((String) profile.get("nickname"))
+                .email((String) kakaoAccount.get("email"))
+                .picture((String) profile.get("profile_image_url"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
-                .name((String) attributes.get("name"))
+                .nickname((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
@@ -50,7 +63,7 @@ public class OAuthAttributes {
         Map<String, Object> response = (Map<String, Object>) attributes.get("response");
 
         return OAuthAttributes.builder()
-                .name((String) response.get("name"))
+                .nickname((String) response.get("nickname"))
                 .email((String) response.get("email"))
                 .picture((String) response.get("profile_image"))
                 .attributes(response)
@@ -62,6 +75,7 @@ public class OAuthAttributes {
         return Member.builder()
                 .email(email)
                 .picture(picture)
+                .nickname(nickname)
                 .memberRole(MemberRole.USER)
                 .joinDate(new SimpleDateFormat("yyyyMMddhhmmss").format(new Date()))
                 .modifyDate(new SimpleDateFormat("yyyyMMddhhmmss").format(new Date()))
