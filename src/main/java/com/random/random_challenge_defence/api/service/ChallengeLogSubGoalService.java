6cc02f3041c92868335e3d13e05b268a7b858d4f
totw5701 <totw5701@naver.com>
2023-08-03 22:54:34 +0900

    feature. 중간목표 수정 기능 재생성

--- src/main/java/com/random/random_challenge_defence/api/service/ChallengeLogSubGoalService.java
+++ src/main/java/com/random/random_challenge_defence/api/service/ChallengeLogSubGoalService.java
@@ -1,10 +1,12 @@
 package com.random.random_challenge_defence.api.service;
 
+import com.random.random_challenge_defence.advice.exception.CAccessDeniedException;
 import com.random.random_challenge_defence.advice.exception.CNotFoundException;
 import com.random.random_challenge_defence.api.dto.challengelog.ChallengeLogSubGoalUpdateDto;
 import com.random.random_challenge_defence.domain.challengelog.ChallengeLogStatus;
 import com.random.random_challenge_defence.domain.challengelogsubgoal.ChallengeLogSubGoal;
 import com.random.random_challenge_defence.domain.challengelogsubgoal.ChallengeLogSubGoalRepository;
+import com.random.random_challenge_defence.domain.member.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,13 +19,20 @@ import java.util.Optional;
 public class ChallengeLogSubGoalService {
 
     private final ChallengeLogSubGoalRepository challengeLogSubGoalRepository;
+    private final MemberService memberService;
 
     public ChallengeLogSubGoal updateSubGoal(ChallengeLogSubGoalUpdateDto reqDto) {
         Optional<ChallengeLogSubGoal> byId = challengeLogSubGoalRepository.findById(reqDto.getId());
         if(!byId.isPresent()) {
-            throw new CNotFoundException("존재하지 않는 subgoal 입니다.");
+            throw new CNotFoundException("존재하지 않는 sub-goal 입니다.");
         }
         ChallengeLogSubGoal challengeLogSubGoal = byId.get();
+
+        Member logOwner = challengeLogSubGoal.getChallengeLog().getMember();
+        if(!memberService.getLoginUserEmail().equals(logOwner.getEmail())) {
+            throw new CAccessDeniedException("권한 없는 요청입니다.");
+        }
+
         challengeLogSubGoal.statusChange(reqDto.getStatus());
         return challengeLogSubGoal;
     }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,17 @@
-insert into S3FILE (id, create_dtm, key, url) values (1, '15920101000000', 'key', 'http://temp.com');
+insert into S3FILE (id, create_dtm, key, url) values (99999L, '15920101000000', 'key', 'https://i.namu.wiki/i/5BMg9O67g2ocAweObDeXri9iimKFDbUvFzppS79WAsFhHXrKAeJ7J0nr5lnHGukryqK6YPHPnTytJvRvo4N8LEqhBc6wWTZF8uUueRT2-WaOtTlIRmij0dHUWscWx07kQdJrFUF22qN2qw932vvhmw.webp');
 
-insert into challenge_card (id, assign_score, description, difficulty, evidence_type, final_goal, title) values (33L, 30, 'desc', 2, 'P', '사진 인증', 'tid3tle');
-insert into challenge_card (id, assign_score, description, difficulty, evidence_type, final_goal, title) values (34L, 30, 'desc', 2, 'P', '사진 인증', 'titleb');
-insert into challenge_card (id, assign_score, description, difficulty, evidence_type, final_goal, title, image_id) values (35L, 30, 'desc', 2, 'P', '사진 인증', 'titlse', 1);
+insert into challenge_card (id, assign_score, description, difficulty, evidence_type, final_goal, title, image_id) values (33L, 30, 'desc', 2, 'P', '사진 인증', 'tid3tle', 99999L);
+insert into challenge_card (id, assign_score, description, difficulty, evidence_type, final_goal, title, image_id) values (34L, 30, 'desc', 2, 'P', '사진 인증', 'titleb', 99999L);
+insert into challenge_card (id, assign_score, description, difficulty, evidence_type, final_goal, title, image_id) values (35L, 30, 'desc', 2, 'P', '사진 인증', 'titlse', 99999L);
 
-insert into challenge_card_sub_goal (id, challenge_card_id, sub_goal) values (1L, 35L, '중간 목표1');
-insert into challenge_card_sub_goal (id, challenge_card_id, sub_goal) values (2L, 35L, '중간 목표2');
-insert into challenge_card_sub_goal (id, challenge_card_id, sub_goal) values (3L, 35L, '중간 목표3');
+insert into challenge_card_sub_goal (id, challenge_card_id, sub_goal) values (1L, 33L, '중간 목표1');
+insert into challenge_card_sub_goal (id, challenge_card_id, sub_goal) values (2L, 33L, '중간 목표2');
+insert into challenge_card_sub_goal (id, challenge_card_id, sub_goal) values (3L, 33L, '중간 목표3');
 
+insert into challenge_card_sub_goal (id, challenge_card_id, sub_goal) values (4L, 34L, '중간 목표1');
+insert into challenge_card_sub_goal (id, challenge_card_id, sub_goal) values (5L, 34L, '중간 목표2');
+insert into challenge_card_sub_goal (id, challenge_card_id, sub_goal) values (6L, 34L, '중간 목표3');
+
+insert into challenge_card_sub_goal (id, challenge_card_id, sub_goal) values (7L, 35L, '중간 목표1');
+insert into challenge_card_sub_goal (id, challenge_card_id, sub_goal) values (8L, 35L, '중간 목표2');
+insert into challenge_card_sub_goal (id, challenge_card_id, sub_goal) values (9L, 35L, '중간 목표3');
